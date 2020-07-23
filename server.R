88ea86782111bf9ea9d6ba102bdcb36391e4e69c
Serene Jiang <31513652+jiangyu1208@users.noreply.github.com>
2020-07-23 01:55:14 -0400

    Update for "Data Exploration" tab
    update for "Data Exploration" tab

--- server.R
+++ server.R
@@ -1,7 +1,43 @@
 library(shiny)
+library(dplyr)
+library(ggplot2)
 
-shinyServer(function(input, output, session){
+math <- read.table("student-mat.csv",sep=";",header=TRUE) 
+
+shinyServer(function(input, output, session) {
+  
+  # Create a new reactive variable
+  newVar <- reactive({
+    newDat <- math %>% filter(school == input$school)
+  })
   
   
+  # Create a scatter plot
+  output$G12Plot <- renderPlot({
+    newDat <- newVar()
+    
+    g <- ggplot(newDat, aes(x = G1, y = G2))
+    
+    if(input$sex){
+      g + geom_point(size = input$size, aes(col = sex))
+    } else {
+      g + geom_point(size = input$size)
+    }
+  })
+  
+  # Create text info
+  output$info <- renderText({
+    newDat <- newVar()
+    
+    paste("The average first period grade for math", input$school, "is", 
+          round(mean(newDat$G1, na.rm = TRUE), 2), 
+          "and the average second period grade is", 
+          round(mean(newDat$G2, na.rm = TRUE), 2), sep = " ")
+  })
+  
+  # Create output of observations
+  output$table <- renderTable({
+    newVar()
+  })
   
 })
--- ui.R
+++ ui.R
@@ -1,24 +1,23 @@
 library(shiny)
 library(shinydashboard)
+library(ggplot2)
 
 ui <- dashboardPage(skin = "blue",
   # Add title                  
-  dashboardHeader(
-    title = "Student Performance Data Analysis",
-    titleWidth = 350),
+  dashboardHeader(title = "Student Performance Data Analysis", titleWidth = 350),
   
   # Define sidebar items
   dashboardSidebar(
     sidebarMenu(
-      menuItem(tabName = "intro", "Introduction", icon = icon("dashboard")),
+      menuItem(tabName = "intro", "Introduction", icon = icon("archive")),
       menuItem(tabName = "data", "Data Exploration", icon = icon("table")),
       menuItem(tabName = "unsuper", "Unsupervised Learning", icon = icon("th")),
-      menuItem(tabName = "model", "Modeling", icon = icon("tablet"))
-    )),
+      menuItem(tabName = "model", "Modeling", icon = icon("tablet")))),
   
   # Define the body of the app
   dashboardBody(
     tabItems(
+      
       # "Introduction" tab
       tabItem(tabName = "intro",
         fluidRow(
@@ -29,25 +28,50 @@ ui <- dashboardPage(skin = "blue",
           column(6,
             # Description of data
             h1("Data Description"),
-            # Box to contain the content
+            # Box to contain the data description content
             box(background = "blue", width = 12,
               h4("This data is from UCI Machine Learning Repository")
               
-              
-              
-            )
-            
-            
-          ),
+    
+            )),
           
           column(6, 
                  # Ability of the APP
-                 h1("Ability of the APP"))
-              )
-    ),
+                 h1("Ability of the APP"),
+                 # Box to contain the ability of this app
+                 box(background = "blue", width = 12,
+                     h4("The control for this app is located on the left 
+                        and the visualizations are given on the right. "))))),
     
     # "Data Exploration" tab
-    tabItem(tabName = "data"),
+    tabItem(tabName = "data",
+            fluidPage(
+              
+              # Application title
+              titlePanel("Data Exploration for Two Schools"),
+              
+              # Sidebar with options for the data set
+              sidebarLayout(
+                sidebarPanel(
+                  h3("Select the Schools:"),
+                  selectizeInput("school", "School", selected = "GP", 
+                                 choices = levels(as.factor(math$school))),
+                  br(),
+                  sliderInput("size", "Size of Points on Graph",
+                              min = 1, max = 10, value = 5, step = 1),
+                  checkboxInput("sex", 
+                                h4("Color Code Gender Status", style = "color:blue;"))
+                ),
+                
+                # Show output
+                mainPanel(
+                  plotOutput("G12Plot"),
+                  textOutput("info"),
+                  tableOutput("table")
+                )
+              )
+            ))
+    ),
     
     # "Unsupervised Learning" tab
     tabItem(tabName = "unsuper"),
@@ -56,4 +80,4 @@ ui <- dashboardPage(skin = "blue",
     tabItem(tabName = "model")
     
   )
- ))
+ )
