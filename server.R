d2dba8966dbe4c3dcee1109d6e261e03da68e3e8
Serene Jiang <31513652+jiangyu1208@users.noreply.github.com>
2020-07-26 22:52:21 -0400

    all models updated

--- server.R
+++ server.R
@@ -69,34 +69,34 @@ shinyServer(function(input, output, session) {
   })
   
 ########################################## PCA ###############################################
-  math <- reactive({
+  math.new <- reactive({
     subset(mathdat, school == input$school, select = c(G1, G2, G3, absences, studytime))
   })
   
   
   output$scores <- renderPrint({
-    X <- math()
+    X <- math.new()
     pca <- princomp(X)
     pca$scores
   })
   
   output$screeplot <- renderPlot(
     {
-      data <- math()
+      data <- math.new()
       dat <- princomp(data)
       screeplot(dat, type = "lines")
     })
   
   output$biplot <- renderPlot(
     {
-      data <- math()
+      data <- math.new()
       dat <- princomp(data)
       biplot(dat, xlabs = rep(".", nrow(data)), cex = 1.2)
     })
   
   output$explain <- renderPlot(
     {
-      data <- math()
+      data <- math.new()
       dat <- princomp(data)
       par(mfrow = c(1, 2))
       plot(dat$sdev^2/sum(dat$sdev^2), xlab = "Principal Component", 
@@ -144,5 +144,133 @@ shinyServer(function(input, output, session) {
     data.frame(mathdat)
   })
   
+################################################ Ensemble models ######################################### 
+  ## convert sona cars1 data variables to factor
   
+  math <- mathdat
+  math$sex <- as.factor(math$sex)
+  math$school <- as.factor(math$school)
+  math$paid <- as.factor(math$paid)
+  
+  
+  ## Input data from the first panel by reactive function
+  
+  selecteddata <- reactive({
+    math[, c(input$var1, input$var2, input$var3)]
+  }) 
+  
+  ## Output function for the plot in first panel
+  
+  output$plot1 <- renderPlot({
+    df1 <- selecteddata()
+    p <- ggplot(df1, aes(x = df1[,1], y = df1[,2], color = df1[,3]))
+    p <- p + geom_point(size = 5) + geom_smooth(method = "lm")
+    p <- p + labs(x = names(df1)[1], y = names(df1)[2], color = names(df1)[3])
+    p
+  })  
+  
+  ## Input data from the second panel by reactive function
+  ## divide the data frame into test and train set
+  
+  cardata <- reactive({
+    df2 <- math[, c(input$var4, input$var5)]
+    set.seed(3)
+    inTrain <- createDataPartition(y = df2[,1], p = 0.75, list = FALSE)
+    traincar <- df2[inTrain,]
+    testcar <- df2[-inTrain,]
+    list(traincar = traincar, testcar = testcar)
+  })
+  
+  ## Reactive function to fit prediction model
+  
+  preddata <- reactive({
+    traincar <- cardata()$traincar
+    testcar <- cardata()$testcar
+    pred1 <- 0
+    pred2 <- 0
+    
+    ## rpart 
+    
+    if(input$showModel1) {
+      formula <- as.formula(paste(input$var4, '~ .' ))
+      set.seed(2376)
+      modelfit <- rpart(formula, method = "class", data = traincar)
+      pred1 <- predict(modelfit, testcar, type = "class")
+    }
+    
+    ## random Forest
+    
+    if(input$showModel2) {
+      x <- traincar[,-1]
+      y <- traincar[,1]
+      set.seed(2376)
+      modelfit2 <- randomForest(x, y, importance = TRUE, proximity = TRUE, ntree = 100)
+      pred2 <- predict(modelfit2, testcar)
+    }
+    
+    list(pred1 = pred1, pred2 = pred2)
+  })
+  
+  ## Function to render plot
+  
+  output$plot2 <- renderPlot({
+    
+    traincar <- cardata()$traincar
+    testcar <- cardata()$testcar
+    p1 <- ggplot()
+    p2 <- ggplot()
+    
+    if(input$showModel1) {
+      pred1 <- preddata()$pred1
+      p1 <- ggplot(testcar, aes(x = testcar[,1], y = pred1))
+      p1 <- p1 + geom_point(size = 5, color = "red", alpha = 0.1)
+      p1 <- p1 + labs(x = names(testcar)[1], y = "Prediction",
+                      title = "Model 1/rpart")
+    }
+    
+    
+    if(input$showModel2) {
+      pred2 <- preddata()$pred2
+      p2 <- ggplot(testcar, aes(x = testcar[,1], y = pred2))
+      p2 <- p2 + geom_point(size = 5, color = "blue", alpha = 0.1)
+      p2 <- p2 + labs(x = names(testcar)[1], y = "Prediction",
+                      title = "Model 2/Random Forest")
+    }
+    
+    return(grid.arrange(p1, p2, ncol = 2))
+    
+  })
+  
+  ## Table to print confusion matrix
+  
+  output$table.3 <- renderTable({
+    
+    traincar <- cardata()$traincar
+    testcar <- cardata()$testcar
+    
+    df3 <- data.frame(rpart = sample(0, 7, replace = TRUE), 
+                      values = sample(0, 7, replace = TRUE))
+    
+    df4 <- data.frame(RandomForest = sample(0, 7, replace = TRUE), 
+                      values = sample(0, 7, replace = TRUE))
+    
+    if(input$showModel1) {
+      pred1 <- preddata()$pred1
+      matrix1 <- confusionMatrix(pred1, testcar[,1])
+      df3 <- data.frame(values = matrix1$overall)
+      df3 <- setDT(df3, keep.rownames = TRUE)[]
+      names(df3)[1] <- "rpart"
+    }
+    
+    if(input$showModel2) {
+      pred2 <- preddata()$pred2
+      matrix2 <- confusionMatrix(pred2, testcar[,1])
+      df4 <- data.frame(values = matrix2$overall)
+      df4 <- setDT(df4, keep.rownames = TRUE)[]
+      names(df4)[1] <- "RandomForest"
+    }
+    
+    cbind(df3, df4)
+  })
+
 })
--- ui.R
+++ ui.R
@@ -188,12 +188,49 @@ ui <- dashboardPage(skin = "blue",
                                   )
                                 )
                               )),
-                      
-                     
+
 ################################ "Ensemble Model" tab ##################################################
-                      tabItem(tabName = "tree"),
-                      
-                     
+                      tabItem(tabName = "tree",
+                              pageWithSidebar(
+                                headerPanel("Ensemble Model"),
+                                sidebarPanel(
+                                  conditionalPanel(condition="input.conditionedPanels==1",
+                                                   h3("Exploratory Plots"),
+                                                   selectInput("var1", "Select variable for x axis", 
+                                                               choices = c("G1", "G2", "G3"), 
+                                                               selected = "G1"),
+                                                   
+                                                   selectInput("var2", "Select variable for y axis", 
+                                                               choices = c("G1", "G2", "G3"), 
+                                                               selected = "G2"),
+                                                   
+                                                   selectInput("var3", "Select a factor variable for color", 
+                                                               choices = c("school", "sex", "paid"), 
+                                                               selected = "school")
+                                  ),
+                                  conditionalPanel(condition="input.conditionedPanels==2",
+                                                   h3("Compare between two models"),
+                                                   selectInput("var4", "Select variable", 
+                                                               choices = c("school", "sex", "paid"), 
+                                                               selected = "school"),
+                                                   
+                                                   selectInput("var5", "Select predictor variables (minimum 2)", 
+                                                               choices = c("G1", "G2", "G3", "school", "sex", "paid"), 
+                                                               selected = c("G1", "G2"), multiple = TRUE),
+                                                   checkboxInput("showModel1", "Show/Hide rpart model", 
+                                                                 value = FALSE),
+                                                   checkboxInput("showModel2", "Show/Hide Random Forest model", 
+                                                                 value = FALSE)
+                                  ) 
+                                ),
+                                mainPanel(
+                                  tabsetPanel(
+                                    tabPanel("Panel 1", plotOutput("plot1"), value = 1), 
+                                    tabPanel("Panel 2", plotOutput("plot2"), tableOutput("table.3"), value = 2)
+                                    , id = "conditionedPanels"
+                                  )
+                                )
+                              )),
 
 ##################################### "Data Page" tab #####################################################
                       tabItem(tabName = "subset")
